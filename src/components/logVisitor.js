5810ab085362aea3d56335377839ff555367685f
credibleinvestmentexperts <invest@credibleinvestmentexperts.com>
2025-09-05 09:17:58 +0100

    update

--- src/components/logVisitor.js
+++ src/components/logVisitor.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 const logVisitor = async (page) => {
   try {
-    const ipResponse = await axios.get("https://api.ipify.org?format=json"); // fetch IP
-    const ip = ipResponse.data.ip;
-
     await axios.post(`${process.env.REACT_APP_BACKEND_URL}api/visitors/all`, {
       page,
-      ip,
     });
   } catch (error) {
     console.error("Failed to log visitor:", error.message);
