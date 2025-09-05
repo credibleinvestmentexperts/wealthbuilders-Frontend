0d1a3f79bb5b036e75383bf86ac545a4cadd4976
credibleinvestmentexperts <invest@credibleinvestmentexperts.com>
2025-09-05 09:34:13 +0100

    update

--- src/components/logVisitor.js
+++ src/components/logVisitor.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const logVisitor = async (page) => {
   try {
+    console.log("Sending visitor log to backend:", page); // 👈 add this
+
     await axios.post(`${process.env.REACT_APP_BACKEND_URL}api/visitors/all`, {
       page,
     });
